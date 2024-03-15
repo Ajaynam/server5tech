459a1fd44e341ae870b3e1a9d9ce328236eb5b98
Ajaynam <ajaynamdev701@gmail.com>
2024-03-15 09:47:15 +0530

    addnew

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const priceRoute = require("./router/priceRouter");
 //Cors Policy
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173" , "*"],
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
   })
